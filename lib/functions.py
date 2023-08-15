d8f3fd00a1fd068b60c8f3c54e685eeeefd4fe82
ubah feisal <feisalubah1@gmail.com>
2023-08-15 10:32:31 +0300

    done

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,24 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello  programmer!")
+greet_programmer()
+    
 
 def greet(name):
-    pass
+    print(f"Hello {name}")
+greet("Ubah")  
+    
 
 def greet_with_default(name="programmer"):
-    pass
+  print(f"Hello {name}")
+# greet_with_default("Zahra")
+greet_with_default()
 
 def add(num1, num2):
-    pass
+   print(num1+num2)
+add(2,5)
 
 def halve(number):
-    pass
+    print(number//2)
+halve(10)
